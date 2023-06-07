3704172cbb34f5ad152342198a9d34718bb18810
Jesús Antonio Gutiérrez Matarrita <jesus.gutierrezm10@outlook.com>
2023-06-06 22:47:11 -0600

    Changes in the way we post a new client

---
+++ investigacion/src/main/kotlin/ucr/ac/cr/investigacion/entity/input/ClientInput.kt
@@ -0,0 +1,11 @@
+package ucr.ac.cr.investigacion.entity.input
+
+import java.util.Date
+
+data class ClientInput(
+    val name: String,
+    val address: String,
+    val phone: String,
+    val email: String,
+    val birthDate: Date
+)
