1141da45ffa14545055844a823613d69415df9f1
Jesús Antonio Gutiérrez Matarrita <jesus.gutierrezm10@outlook.com>
2023-06-07 16:22:42 -0600

    ClientController can now Add, Update and Delete

---
+++ investigacion/src/main/kotlin/ucr/ac/cr/investigacion/controller/ClientController.kt
@@ -0,0 +1,75 @@
+package ucr.ac.cr.investigacion.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
+import ucr.ac.cr.investigacion.entity.Client
+import ucr.ac.cr.investigacion.entity.input.ClientInput
+import ucr.ac.cr.investigacion.repository.ClientRepository
+import java.util.*
+
+@Controller
+class ClientController @Autowired constructor(private val clientRepository : ClientRepository) {
+
+    @QueryMapping
+    fun clients() : Iterable<Client> {
+        return clientRepository.findAll()
+    }
+
+    @QueryMapping
+    fun clientById(@Argument id : Int) : Optional<Client> {
+        return clientRepository.findById(id);
+    }
+
+    @MutationMapping
+    fun addClient(@Argument clientInput : ClientInput) : Client {
+        val client = Client(
+            name = clientInput.name,
+            address = clientInput.address,
+            phone = clientInput.phone,
+            email = clientInput.email,
+            birthDate = clientInput.birthDate
+        )
+
+        return clientRepository.save(client);
+    }
+
+    @MutationMapping
+    fun updateClient(@Argument id: Int, @Argument clientInput: ClientInput) : Optional<Client> {
+        val existingClientOptional = clientById(id)
+        if (existingClientOptional.isPresent) {
+            val existingClient = existingClientOptional.get()
+
+            // Create a new client with updated properties
+            val updatedClient = existingClient.copy(
+                name = clientInput.name ?: existingClient.name,
+                address = clientInput.address ?: existingClient.address,
+                phone = clientInput.phone ?: existingClient.phone,
+                email = clientInput.email ?: existingClient.email,
+                birthDate = clientInput.birthDate ?: existingClient.birthDate
+            )
+
+            val savedClient = clientRepository.save(updatedClient)
+            return Optional.of(savedClient)
+        } else {
+            return Optional.empty()
+        }
+
+    }
+
+    @MutationMapping
+    fun deleteClient(id: Int) : Boolean {
+        val clientOptional = clientById(id)
+
+        if(clientOptional.isPresent) {
+            clientRepository.deleteById(id)
+            return true
+        }
+
+        return false
+    }
+
+
+}
--- investigacion/src/main/kotlin/ucr/ac/cr/investigacion/entity/input/ClientInput.kt
+++ investigacion/src/main/kotlin/ucr/ac/cr/investigacion/entity/input/ClientInput.kt
@@ -1,8 +1,8 @@
 package ucr.ac.cr.investigacion.entity.input
 
 import java.util.Date
-
 data class ClientInput(
+
     val name: String,
     val address: String,
     val phone: String,
