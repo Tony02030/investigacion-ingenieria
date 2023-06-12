cf901c43f47817cab5b3a9f1925a44c1bef3713a
Tony02030 <ANTHONY.RODRIGUEZSOTO@ucr.ac.cr>
2023-06-11 18:06:01 -0600

    Revert "Merge branch 'main' into post-graphql"
    This reverts commit f1f2d9aef00865a82a0531676c1ea7a437f1c791, reversing
    changes made to 94f84bfdfc3ba29c28dea9abeb910f99c46ab6c5.

--- investigacion/src/main/kotlin/ucr/ac/cr/investigacion/controller/ClientController.kt
+++ investigacion/src/main/kotlin/ucr/ac/cr/investigacion/controller/ClientController.kt
@@ -7,9 +7,6 @@ import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseBody
 import ucr.ac.cr.investigacion.entity.Client
 import ucr.ac.cr.investigacion.service.ClientService
 import java.lang.IllegalArgumentException
@@ -19,13 +16,6 @@ import java.util.*
 class ClientController @Autowired constructor(
     private val clientService: ClientService
 ) {
-
-    @PostMapping("/client")
-    @ResponseBody
-    fun createClient(@RequestBody client: Client): Client {
-        val createClient = clientRepository.save(client)
-        return createClient
-    }
 
     @QueryMapping
     fun clients(): Iterable<Client> {
